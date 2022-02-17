dba803ffeed8911a463bdf951b161634a914bae1
Jesús Caro <jfcarocota@gmail.com>
2022-02-16 20:27:48 -0700

    publicación de tareas
    Generé una ruta para crear nuevas tareas, estas tareas de guardan dentro de una arreglo llamado task

--- app.py
+++ app.py
@@ -1,27 +1,19 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for, redirect
+from database import tasks
 
 app = Flask(__name__)
 
 #si tu ni le dices a route que tipo de metodo http va usar, por default usará un GET
 @app.route("/")
 def Index():
-  return render_template("index.html")
-  #return "hello world flask"
-  #return "<h1>hello world flask</h1>"
-  #return "<a href='#'>hello world flask</a>"
+  return render_template("index.html", todoList = tasks)
 
-#@app.route("/ebc")
-#def HolaEBC():
-#  return "Hola ebc"
-#
-#@app.route("/ebc/random")
-#def UnGetRandom():
-#  return "Hola ebc random"
-
-#@app.route("/", methods=['POST'])
-#def AddNewTask():
-#  if request.method == 'POST':
-    
+@app.route("/newTask", methods=['POST'])
+def AddNewTask():
+  if request.method == 'POST':
+    newTask = {"id": len(tasks) + 1, "name": request.form['task_name']}
+    tasks.append(newTask)
+    return redirect(url_for('Index'))
 
 if __name__ == "__main__":
   app.run(port=5000, debug=True)
